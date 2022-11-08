1e226b99326b8da58c85fd459a2025f2031a7fa4
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2022-11-08 16:19:41 +0900

    Create 2056.py
    2056. 연월일 달력 
    해당 날짜의 유효성을 판단한 후, 날짜가 유효하다면 ”YYYY/MM/DD”형식으로 출력하고, 날짜가 유효하지 않을 경우, -1 을 출력하는 프로그램을 작성하라.

---
+++ SWEA/2056.py
@@ -0,0 +1,15 @@
+T = int(input())
+a = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+for test_case in range(1, T + 1):
+    case = str(input())
+    yy = case[0:4]
+    mm = case[4:6]
+    dd = case[6:8]
+    
+    answer = ""
+    if 0 < int(mm) < 13 and 0 < int(dd) <= a[int(mm)]:
+        answer = yy + '/' + mm + '/' + dd
+    else:
+        answer += '-1'
+
+    print("#" + str(test_case) + " " + answer)
