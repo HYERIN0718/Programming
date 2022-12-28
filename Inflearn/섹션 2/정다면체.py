40ebe9e492348ff04d529bbd6c1032b6dfe8a529
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2022-12-29 00:04:25 +0900

    Create 정다면체.py
    두 개의 정 N면체와 정 M면체의 두 개의 주사위를 던져서 나올 수 있는 눈의 합 중 가장 확률이 높은 숫자를 출력하는 프로그램을 작성하세요. 정답이 여러 개일 경우 오름차순으로 출력합니다.

---
+++ Inflearn/섹션 2/정다면체.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin=open("input.txt", "rt")
+
+a, b = map(int, input().split())
+cnt = [0]*(a + b + 3)
+aMax = 0
+
+# 주사위 두 개를 던져서 나올 수 있는 수 모두 구하기
+for i in range(1, a + 1):
+    for j in range(1, b + 1):
+        cnt[i + j] += 1
+
+# 확률이 높은 숫자
+for i in range(a + b + 1):
+    if cnt[i] > aMax:
+        aMax = cnt[i]
+
+# 오름차순으로 출력
+for i in range(a + b + 1):
+    if cnt[i] == aMax:
+        print(i, end=' ')
