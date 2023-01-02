811049b623e130f95f131b66b8eea05f53cdf788
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2023-01-02 18:29:52 +0900

    Create 뒤집은_소수.py
    N개의 자연수가 입력되면 각 자연수를 뒤집은 후 그 뒤집은 수가 소수이면 그 수를 출력하는 프로그램을 작성하세요. 단, 숫자를 뒤집었을 때 0으로 시작하면 첫 자리부터의 연속된 0은 무시한다.

---
+++ Inflearn/섹션 2/뒤집은_소수.py
@@ -0,0 +1,35 @@
+import sys
+sys.stdin=open("input.txt", "rt")
+
+# 각 자연수를 뒤집기
+def reverse(x):
+    res = 0
+    while x > 0:
+        t = x % 10
+        
+        # 뒤집기
+        res = res * 10 + t
+        x = x // 10
+    return res
+
+# 뒤집은 숫자가 소수인지 판별하기
+def isPrime(x):
+  
+    # 1은 소수가 아니기 때문에 제외시킴
+    if x == 1:
+        return False
+      
+    # 소수는 입력받은 숫자의 절반까지 해당됨
+    for i in range(2, x // 2 + 1):
+        if x % i == 0:
+            return False
+        else:
+            return True
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+for x in arr:
+    tmp = reverse(x)
+    if isPrime(tmp):
+        print(tmp, end = ' ')
