1bb93cde2020952de999603c164510d416611a28
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2023-01-02 17:23:38 +0900

    Create 소수의_개수(에라토스테네스 체).py
    자연수 N이 입력되면 1부터 N까지의 소수의 개수를 출력하는 프로그램을 작성하세요.

---
+++ Inflearn/섹션 2/소수의_개수(에라토스테네스 체).py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin=open("input.txt", "rt")
+
+n = int(input())
+
+# n까지 반복해야하므로 배열의 크기를 n + 1 만큼 초기화함
+arr = [0] * (n + 1)
+cnt = 0
+
+for i in range(2, n + 1):
+    if arr[i] == 0:
+        cnt += 1
+        
+        # if문 안의 for문 -> 값이 0인 인덱스의 배수 값에 1을 넣어야 하기 때문
+        # range(i, n+1, i) -> i 만큼 증가 : i 배수 값 
+        for j in range(i, n + 1, i):
+            arr[j] = 1
+
+print(cnt)
