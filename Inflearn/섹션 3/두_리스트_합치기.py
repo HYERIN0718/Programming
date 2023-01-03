027fcb2322fd2ee8090a4030e9048981cbfb3a62
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2023-01-03 15:33:29 +0900

    Update 두_리스트_합치기.py

--- Inflearn/섹션 3/두_리스트_합치기.py
+++ Inflearn/섹션 3/두_리스트_합치기.py
@@ -6,7 +6,30 @@ a = list(map(int, input().split()))
 m = int(input())
 b = list(map(int, input().split()))
 
-arr = []
+p1 = p2 = 0
+c=[]
+
+# p1이나 p2 둘 중 하나라도 n, m 과 같아질 때까지 반복함
+while p1 < n and p2 < m:
+    if a[p1] <= b[p2]:
+        c.append(a[p1])
+        p1 += 1
+    else:
+        c.append(b[p2])
+        p2 += 1
+
+# n이 m보다 크다면 a리스트를 추가하고 m이 n보다 크다면 b리스트를 추가함
+if p1 < n:
+    c = c + a[p1:]
+if p2 < m:
+    c = c + b[p2:]
+
+for i in c:
+    print(i, end = ' ')
+
+
+# 내가 푼 답 (sort()로 풀게되면 nlogn횟수 반복하므로 비효율적임)
+'''arr = []
 for i in a:
     arr.append(i)
 
@@ -15,4 +38,4 @@ for i in b:
 arr.sort()
 
 for j in arr:
-    print(j, end=' ')
+    print(j, end=' ')'''
