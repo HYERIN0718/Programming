b3775633afac2a75286268fa990bb0e133828d91
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2023-01-03 14:37:08 +0900

    Update 숫자만_추출.py

--- Inflearn/섹션 3/숫자만_추출.py
+++ Inflearn/섹션 3/숫자만_추출.py
@@ -1,8 +1,14 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
+n = input()
+s = 0
+for i in n:
+    if i.isdecimal():
+        s = s * 10 + int(i)
 
-n = list(map(str, str(input())))
+# 내가 푼 답
+''' n = list(map(str, str(input())))
 a = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 arr = []
 
@@ -10,7 +16,7 @@ for i in range(len(n)):
     if n[i] in a:
         arr.append(n[i])
 arr = ''.join(arr)
-s = int(arr)
+s = int(arr)'''
     
 print(s)
 
