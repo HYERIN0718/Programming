3bfe11925c8ce00b2ab60915df43a5b1e6845920
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2023-01-03 14:26:17 +0900

    Create 숫자만_추출.py
    문자와 숫자가 섞여있는 문자열이 주어지면 그 중 숫자만 추출하여 그 순서대로 자연수를 만듭니다. 만들어진 자연수와 그 자연수의 약수 개수를 출력합니다.

---
+++ Inflearn/섹션 3/숫자만_추출.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin=open("input.txt", "rt")
+
+
+n = list(map(str, str(input())))
+a = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+arr = []
+
+for i in range(len(n)):
+    if n[i] in a:
+        arr.append(n[i])
+arr = ''.join(arr)
+s = int(arr)
+    
+print(s)
+
+count = 0
+for i in range(1, s + 1):
+    if s % i == 0:
+        count += 1
+print(count)
