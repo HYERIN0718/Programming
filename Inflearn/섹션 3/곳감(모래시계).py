f55b46e08eb5b811d9c96382546d9e594cec00dc
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2023-01-05 16:46:48 +0900

    Create 곳감(모래시계).py
     N*N 격자판의 첫 번째 수는 행번호, 두 번째 수는 방향인데 0이면 왼쪽, 1이면 오른쪽이고, 세 번째 수는 회전하는 격자의 수입니다. M개의 회전명령을 실행하고 난 후 아래와 같이 마당의 모래시계 모양의 영역에는 감이 총 몇 개가 있는지 출력하는 프로그램을 작성하세요.

---
+++ Inflearn/섹션 3/곳감(모래시계).py
@@ -0,0 +1,33 @@
+import sys
+sys.stdin=open("input.txt", "rt")
+
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
+m = int(input())
+
+# 회전
+for i in range(m):
+    b, c, d = map(int, input().split())
+    for j in range(d):
+        if c == 0:
+            # pop(0) : 0번 인덱스 값 출력
+            a[b-1].append(a[b-1].pop(0))
+        else:
+            # pop() : 마지막 인덱스 값 출력
+            a[b-1].insert(0, a[b-1].pop())
+
+count = 0
+s = 0
+e = n - 1 
+
+# 모래시계 모양 값 출력
+for i in range(n):
+    for j in range(s, e+1):
+        count += a[i][j]
+    if i < n//2:
+        s += 1
+        e -= 1
+    else:
+        s -= 1
+        e += 1 
+print(count)
