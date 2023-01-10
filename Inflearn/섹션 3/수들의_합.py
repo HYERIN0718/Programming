1e01fe479e95c95e582f91d7ed70a460563bb8a9
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2023-01-10 17:53:00 +0900

    Update 수들의_합.py

--- Inflearn/섹션 3/수들의_합.py
+++ Inflearn/섹션 3/수들의_합.py
@@ -11,6 +11,8 @@ count = 0
 while True:
     if s < b:
         if rt < a:
+            
+            # s값은 lt ~ rt-1 인덱스의 값 까지의 합
             s += arr[rt]
             rt += 1
             
@@ -19,8 +21,8 @@ while True:
             break
     elif s == b:
         count += 1
-        
-        # 앞의 인덱스 값을 유지하기 위해 lt값을 삭제함
+
+        # 인덱스 값을 증가시키면서 새로운 경우의 수를 구하기 위해 lt값을 삭제(초기화)함
         s -= arr[lt]
         lt += 1
     else:
