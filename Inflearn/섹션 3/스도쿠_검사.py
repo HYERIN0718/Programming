457350df36528aa0541082f153bb2c47b120c684
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2023-01-11 23:19:17 +0900

    Update 스도쿠_검사.py

--- Inflearn/섹션 3/스도쿠_검사.py
+++ Inflearn/섹션 3/스도쿠_검사.py
@@ -23,6 +23,20 @@ def check(a):
             if sum(ch3) != 9:
                 return False
     return True
+  
+    '''범위 수정
+    # 3x3칸씩 1~9 맞는지 확인
+    for i in range(0, 9, 3):
+        for j in range(0, 9, 3):
+            ch3 = [0] * 10
+            for k in range(3):
+                for s in range(3):
+                
+                    # i와 j 범위를 9(3씩 이동)까지 설정하면 *3을 해주지 않아도 됨
+                    ch3[a[i+k][j+s]] = 1
+            if sum(ch3) != 9:
+                return False
+    return True '''
 
 a = [list(map(int, input().split())) for _ in range(9)]
 if check(a):
