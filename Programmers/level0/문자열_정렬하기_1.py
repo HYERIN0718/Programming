2b522810f69ad813d52fc8ce33746568b97e5ca2
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2023-01-13 21:41:37 +0900

    Update 문자열_정렬하기_1.py
    문자열 my_string이 매개변수로 주어질 때, my_string 안에 있는 숫자만 골라 오름차순 정렬한 리스트를 return 하도록 solution 함수를 작성해보세요.

--- Programmers/level0/문자열_정렬하기_1.py
+++ Programmers/level0/문자열_정렬하기_1.py
@@ -1,10 +1,10 @@
 def solution(my_string):
     answer = []
-    arr = list(map(str, str(my_string)))
-    num = ['0','1','2','3','4','5','6','7','8','9']
     
-    for i in range(len(arr)):
-        if arr[i] in num:
-            answer.append(int(arr[i]))
+    for i in range(len(my_string)):
+        if my_string[i].isdigit():
+            answer.append(int(my_string[i])
+                          
     answer.sort()
+                          
     return answer
