aad739ee9f82e5871b788c6f69eb1b70f865a823
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2023-01-23 21:16:37 +0900

    Update 침몰하는_타이타닉(그리디).py

--- Inflearn/섹션 4/침몰하는_타이타닉(그리디).py
+++ Inflearn/섹션 4/침몰하는_타이타닉(그리디).py
@@ -1,4 +1,34 @@
 import sys
+from collections import deque  # deque 자료구조로 풀기
+sys.stdin=open("input.txt", "rt")      
+
+n, m = map(int, input().split())
+arr = list(map(int,input().split()))
+
+arr.sort()
+arr = deque(arr)  # 리스트를 deque 형태로 변환
+count = 0
+
+while arr:
+    if len(arr) == 1:
+        count += 1
+        break
+    
+    if arr[0] + arr[-1] > m:
+        arr.pop()
+        count += 1
+    else:
+      
+        # 배열에서 제일 왼쪽에 있는 값 제거
+        arr.popleft()
+        arr.pop(0)
+        count += 1
+
+print(count)
+
+
+# deque 자료구조로 풀지 않았을 때 
+'''import sys
 sys.stdin=open("input.txt", "rt")      
 
 n, m = map(int, input().split())
@@ -23,4 +53,4 @@ while arr:
         arr.pop(0)
         count += 1
 
-print(count)
+print(count)'''
