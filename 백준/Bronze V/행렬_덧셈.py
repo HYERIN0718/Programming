49bbddc74e02b0b0cb3b013389752f24b8c72a15
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2023-01-26 14:23:30 +0900

    Create 행렬_덧셈.py
    n * m 크기의 두 행렬 a와 b가 주어졌을 때, 두 행렬을 더하는 프로그램을 작성하세요.

---
+++ 백준/Bronze V/행렬_덧셈.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+
+a, b = [], []
+
+for i in range(n):
+    arr = list(map(int, input().split()))
+    a.append(arr)
+for i in range(n):
+    arr = list(map(int, input().split()))
+    b.append(arr)
+
+for i in range(n):
+    for j in range(m):
+        print(a[i][j] + b[i][j], end= ' ')
+    print()
