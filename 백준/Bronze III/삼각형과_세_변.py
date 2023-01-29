dd71f59978057de6bb7c4cf74f01ccbf9f9c6889
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2023-01-29 17:45:14 +0900

    Update 삼각형과_세_변.py

--- 백준/Bronze III/삼각형과_세_변.py
+++ 백준/Bronze III/삼각형과_세_변.py
@@ -2,7 +2,7 @@ while True:
     a = sorted(list(map(int, input().split())))
     if a[0] == a[1] == a[2] == 0:
         break
-    if a[0] + a[1] <= a[2]:
+    if a[0] + a[1] <= a[2]: # 세 변의 길이가(2, 2, 8)일 때 삼각형이 만들어지지 않으므로 Invalid를 출력해서 오류 방지
         print("Invalid")
     elif a[0] == a[1] == a[2]:
         print("Equilateral")
